07319b4bbeb1e58d32d30400473cefee8c228168
Yuri Uzun <y.uzun@dextechnology.com>
2020-08-06 09:18:28 +0300

    change post/put request to mongoose

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,10 +4,23 @@ const mongoose = require("mongoose")
 
 const Product = require("../models/product");
 
+
 router.get('/', (req,res,next) => {
 
-  res.status(200).json({
-    message:"Handling GET requests to /products"
+  Product.find().then(docs => {
+    console.log(docs);
+    if(docs.length >=0) {
+      res.status(200).json(docs)
+    } else {
+      res.status(404).json({
+        message:"No entries found"
+      })
+    }
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error:err
+    })
   })
 });
 
@@ -20,18 +33,26 @@ _id: new mongoose.Types.ObjectId(),
 
 })
 
-  product.save().then(result => console.log(result)).catch(err => console.log(err));
+  product.save().then(result => {
+    console.log(result);
+    res.status(201).json({
+      message:"Handling POST requests to /products",
+      createProduct: result
 
-  res.status(201).json({
-    message:"Handling POST requests to /products",
-    createProduct: product
+    })
+  }).catch(err => {
+    console.log(err)
+    res.status(500).json({
+      error:err
+    })
+  });
 
-  })
+
 });
 
 router.get('/:productId', ((req, res, next) => {
   const id = req.params.productId;
-  Product.findById(id)
+  Product.findById(id).exec()
       .then(doc=> {
         console.log("From database",doc);
         res.status(200).json(doc)
@@ -47,14 +68,34 @@ router.get('/:productId', ((req, res, next) => {
 
 
 router.patch('/:productId', ((req, res, next) => {
-res.status(200).json({
-  message:"Updated product"
-})
+  const id = req.params.productId;
+  const updateOps = {};
+  for(const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({_id:id}, {$set: updateOps}).then(result => {
+    console.log(result);
+    res.status(200).json(result);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error:err
+    });
+
+
+  })
+
 }))
 
 router.delete('/:productId', ((req, res, next) => {
-  res.status(200).json({
-    message:"Deleted product"
+  const id = req.params.productId;
+  Product.remove({_id:id}).then(result => {
+    res.status(200).json(result)
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error:err
+    })
   })
 }))
 
